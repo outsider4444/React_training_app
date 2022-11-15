33b4f8a8e790c955892937eddcd5ec5bfdf77bc0
Danila Kuzin <danila_kuzin_2001@mail.ru>
2022-11-15 19:28:27 +0400

    Добавление перехода по ссылкам (Route). Мелкие правки в стилях. Добавление нескольких страничек для тестов

---
+++ src/components/Navbar/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return(
+        <nav className={style.nav}>
+            <div>
+                <NavLink to="/profile/" className = { navData => navData.isActive ? style.active : style.item }>Profile</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/dialogs/" className = { navData => navData.isActive ? style.active : style.item }>Messages</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={"/news/"} className = { navData => navData.isActive ? style.active : style.item }>News</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={"/music/"} className = { navData => navData.isActive ? style.active : style.item }>Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={"/settings/"} className = { navData => navData.isActive ? style.active : style.item }>Settings</NavLink>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
